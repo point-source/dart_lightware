3d8b8319dd19bd0cb9ca06bc38688e9fd14a84aa
PointSource <47544779+point-source@users.noreply.github.com>
2022-09-30 16:07:28 -0700

    Init version that can send base request types

--- example/dart_lightware_example.dart
+++ example/dart_lightware_example.dart
@@ -1,6 +1,16 @@
 import 'package:dart_lightware/dart_lightware.dart';
 
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+Future<void> main() async {
+  var lightware = Lightware('10.10.8.106');
+
+  final r = await lightware.get(
+    '/MEDIA/PORTS/VIDEO/STATUS/I7',
+    property: 'SignalPresent',
+  );
+
+  r.forEach(print);
+
+  final c = await lightware.call('/MEDIA/XP/VIDEO', 'switch', ['I7:O13']);
+
+  c.forEach(print);
 }
--- lib/src/dart_lightware_base.dart
+++ lib/src/dart_lightware_base.dart
@@ -1,18 +1,101 @@
+import 'dart:async';
+
+import 'package:tcp_client_dart/tcp_client_dart.dart';
+
 class Lightware {
   Lightware(this.host, {this.port = 6107});
 
   final String host;
   final int port;
 
-  String _get(String path, String method) {}
+  TcpClient? _client;
 
-  String _set(String path, String method, String values) {}
+  Future<TcpClient> get client async {
+    if (_client == null) {
+      _client = await TcpClient.connect(
+        host,
+        port,
+        terminatorString: '\r\n',
+        connectionType: TcpConnectionType.persistent,
+      );
 
-  String _call(String path, String method, String params) {}
+      _client!.stringStream.listen(_handleEvent);
+    }
+
+    return _client!;
+  }
+
+  int _requestCount = 0;
+
+  /// The signature is a four-digit-long hexadecimal value that can be optionally placed before
+  /// every command to keep a command and the corresponding responses together as a group.
+  String _getSignature() {
+    if (_requestCount == 65535) _requestCount = 0;
+
+    return (_requestCount++).toRadixString(16).padLeft(4, '0').toUpperCase();
+  }
+
+  final Map<String, Completer<List<String>>> _requests = {};
+
+  final List<String> _responseCache = [];
+
+  void _handleEvent(String event) {
+    if (event.startsWith('{')) {
+      _responseCache.clear();
+    } else if (event.startsWith('}')) {
+      final signature = _responseCache.removeAt(0).substring(1, 5);
+      _requests.remove(signature)?.complete(_responseCache.toList());
+      _responseCache.clear();
+
+      return;
+    }
+    _responseCache.add(event);
+  }
+
+  /// Sends a raw command string to the Lightware device and returns
+  /// any responses as a list of strings (signature and brackets removed)
+  Future<List<String>> sendRaw(String command) async {
+    final signature = _getSignature();
+    final completer = Completer<List<String>>();
+    _requests[signature] = completer;
+    (await client).send('$signature#$command');
+
+    return await completer.future;
+  }
+
+  Future<List<String>> get(String path, {String property = ''}) async {
+    final command = property.isEmpty ? 'GET $path' : 'GET $path.$property';
+
+    return await sendRaw(command);
+  }
+
+  Future<List<String>> set(String path, String property, String values) async {
+    final command = 'SET $path.$property=$values';
+
+    return await sendRaw(command);
+  }
+
+  Future<List<String>> call(
+    String path,
+    String method,
+    List<String> params,
+  ) async {
+    final command = 'CALL $path:$method(${params.join(";")})';
+
+    return await sendRaw(command);
+  }
+
+  Future<List<String>> man(String path, String property) async {
+    final command = property.isEmpty ? 'MAN $path' : 'MAN $path.$property';
+
+    return await sendRaw(command);
+  }
+
+  Future<void> disconnect() async {
+    await _client?.close();
+    _requests.clear();
+    _requestCount = 0;
+    _responseCache.clear();
+    _client = null;
+  }
 }
-
-class Response {
-  Prefix prefix;
-}
-
-enum Prefix { success, error, readOnly }
